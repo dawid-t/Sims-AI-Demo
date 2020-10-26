ad50ea22e0b4deadec2cc7af112452d20cb5be9b
dawid-t <dawidt@protonmail.com>
2020-10-26 03:24:14 +0100

    Add PlayerStatistics script to the player

---
+++ Assets/Scripts/PlayerStatistics.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStatistics : MonoBehaviour
+{
+	[SerializeField]
+	private int maxStatisticValue = 10, startStatisticValue = 5;
+	[SerializeField] [Tooltip("Set time in seconds.")]
+	private int statisticsDecreaseTime = 30;
+	private int hunger, entertainment, hygiene, bladder, energy;
+
+
+	private void Start()
+	{
+		InitStatistics();
+		StartCoroutine(DecreaseStatisticsOverTime());
+	}
+
+	private void OnValidate()
+	{
+		CheckStartStatisticValue();
+	}
+
+	private void InitStatistics()
+	{
+		CheckStartStatisticValue();
+
+		hunger = startStatisticValue;
+		entertainment = startStatisticValue;
+		hygiene = startStatisticValue;
+		bladder = startStatisticValue;
+		energy = startStatisticValue;
+	}
+
+	private void CheckStartStatisticValue()
+	{
+		if(startStatisticValue > maxStatisticValue)
+		{
+			startStatisticValue = maxStatisticValue;
+		}
+	}
+
+	private IEnumerator DecreaseStatisticsOverTime()
+	{
+		while(true)
+		{
+			yield return new WaitForSeconds(statisticsDecreaseTime);
+
+			if(hunger > 0)
+			{
+				hunger--;
+			}
+			if(entertainment > 0)
+			{
+				entertainment--;
+			}
+			if(hygiene > 0)
+			{
+				hygiene--;
+			}
+			if(bladder > 0)
+			{
+				bladder--;
+			}
+			if(energy > 0)
+			{
+				energy--;
+			}
+
+			if(hunger == 0 && entertainment == 0 && hygiene == 0 && bladder == 0 && energy == 0)
+			{
+				// TODO: Play death anim and restart the scene.
+				Debug.Log("DEAD!!!");
+				break;
+			}
+		}
+	}
+}

a8fcefc9b360418c1b6a0732d203ea6c5b63e342
dawid-t <dawidt@protonmail.com>
2020-10-26 02:14:02 +0100

    Add UsableObject script & add it to the bed

--- Assets/Scripts/PlayerInteraction.cs
+++ Assets/Scripts/PlayerInteraction.cs
@@ -6,10 +6,15 @@ using UnityEngine.EventSystems;
 
 public class PlayerInteraction : MonoBehaviour
 {
+	private bool isPlayerUsingObject = false;
 	private Camera mainCamera;
 	private Animator animator;
 	private NavMeshAgent navMeshAgent;
-	private Coroutine StopWalkAnimationCoroutine;
+	private Coroutine stopWalkAnimationCoroutine;
+	private UsableObject lastUsableObject;
+
+
+	public bool IsPlayerUsingObject { get => isPlayerUsingObject; set => isPlayerUsingObject = value; }
 
 
 	private void Start()
@@ -25,6 +30,7 @@ public class PlayerInteraction : MonoBehaviour
 		{
 			return;
 		}*/
+
 		CheckHoveredObjectAndInteract(); // Do something if hovered object is interactable.
 	}
 
@@ -37,21 +43,29 @@ public class PlayerInteraction : MonoBehaviour
 		{
 			GameObject hoveredGameObject = hit.transform.gameObject;
 
-			if(hoveredGameObject.CompareTag("WalkableGround"))
+			if(!isPlayerUsingObject)
 			{
-				if(Input.GetMouseButtonUp(0))
+				if(hoveredGameObject.CompareTag("WalkableGround"))
 				{
-					WalkToClickedPoint(hit.point);
+					if(Input.GetMouseButtonUp(0))
+					{
+						WalkToClickedPoint(hit.point);
+					}
+				}
+				else if(hoveredGameObject.CompareTag("UsableObject") || hoveredGameObject.CompareTag("Player"))
+				{
+					lastUsableObject = hoveredGameObject.GetComponent<UsableObject>();
+					ShowInteractionInfo(lastUsableObject);
+					if(Input.GetMouseButtonUp(0))
+					{
+						WalkToClickedPoint(lastUsableObject.StartUsingObjectPosition.position);
+						DoInteraction(lastUsableObject);
+					}
 				}
 			}
-			else if(hoveredGameObject.CompareTag("UsableObject") || hoveredGameObject.CompareTag("Player"))
+			else if(Input.GetMouseButtonUp(0))
 			{
-				ShowInteractionInfo(hoveredGameObject);
-
-				if(Input.GetMouseButtonUp(0))
-				{
-					DoInteraction(hoveredGameObject);
-				}
+				StopInteraction(lastUsableObject); // TEraz sie nadpisuje a ma dodawac sie do kolejki!
 			}
 		}
 	}
@@ -64,33 +78,38 @@ public class PlayerInteraction : MonoBehaviour
 			animator.SetTrigger("Walk");
 		}
 
-		if(StopWalkAnimationCoroutine != null)
+		if(stopWalkAnimationCoroutine != null)
 		{
-			StopCoroutine(StopWalkAnimationCoroutine);
-			StopWalkAnimationCoroutine = null;
+			StopCoroutine(stopWalkAnimationCoroutine);
+			stopWalkAnimationCoroutine = null;
 		}
-		StartCoroutine(StopWalkAnimation());
+		stopWalkAnimationCoroutine = StartCoroutine(StopWalkAnimation());
 	}
 
 	private IEnumerator StopWalkAnimation()
 	{
 		yield return null; // Need to wait 1 frame to update the "navMeshAgent.remainingDistance" value.
-		while(navMeshAgent.remainingDistance > 0.5f)
+		while(navMeshAgent.enabled && navMeshAgent.remainingDistance > 0.5f)
 		{
 			yield return null;
 		}
 
 		animator.SetTrigger("Idle");
-		StopWalkAnimationCoroutine = null;
+		stopWalkAnimationCoroutine = null;
 	}
 
-	private void ShowInteractionInfo(GameObject hoveredGameObject)
+	private void ShowInteractionInfo(UsableObject usableObject)
 	{
-
+		//show usableObject.ObjectInteractionInfo
 	}
 
-	private void DoInteraction(GameObject hoveredGameObject)
+	private void DoInteraction(UsableObject usableObject)
 	{
-		
+		usableObject.StartUsingObject(gameObject);
+	}
+
+	private void StopInteraction(UsableObject usableObject)
+	{
+		usableObject?.StopUsingObject(gameObject);
 	}
 }
---
+++ Assets/Scripts/UsableObjects/UsableObject.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class UsableObject : MonoBehaviour
+{
+	[SerializeField]
+	protected int usableTime, delayedUsableTime;
+	[SerializeField] [Range(-5, 5)]
+	protected int hungerChanger, entertainmentChanger, hygieneChanger, bladderChanger, energyChanger;
+	[SerializeField]
+	protected string objectInteractionInfo, startPlayerAnimationTriggerName, endPlayerAnimationTriggerName;
+	[SerializeField]
+	protected int startPlayerAnimationRotation;
+	[SerializeField]
+	protected Transform startUsingObjectPosition;
+	protected Animator animator;
+	protected Coroutine usingObjectCoroutine;
+
+
+	public string ObjectInteractionInfo => objectInteractionInfo;
+	public Transform StartUsingObjectPosition => startUsingObjectPosition;
+
+
+	private void Start()
+	{
+		animator = GetComponent<Animator>();
+	}
+
+	public void StartUsingObject(GameObject player)
+	{
+		if(usingObjectCoroutine == null)
+		{
+			usingObjectCoroutine = StartCoroutine(UsingObject(player));
+		}
+	}
+
+	protected IEnumerator UsingObject(GameObject player)
+	{
+		// Wait for the player to reach the correct position:
+		while((player.transform.position - startUsingObjectPosition.position).magnitude > 0.1f)
+		{
+			yield return new WaitForSeconds(0.1f);
+		}
+
+		// Play the player animation:
+		StartCoroutine(PlayAnimations(player, true));
+
+		// You can override the method and add other animations of other objects:
+		PlayOtherAnimations();
+
+		// Change the player properties over time:
+		yield return new WaitForSeconds(delayedUsableTime);
+		int currentUsableTime = 0;
+		while(currentUsableTime < usableTime)
+		{
+			yield return new WaitForSeconds(1);
+
+			// update player properties and UI
+
+			currentUsableTime++;
+		}
+
+		// End using the object:
+		StopUsingObject(player);
+	}
+
+	public void StopUsingObject(GameObject player)
+	{
+		if(usingObjectCoroutine != null)
+		{
+			StopCoroutine(usingObjectCoroutine);
+			usingObjectCoroutine = null;
+			
+			StartCoroutine(PlayAnimations(player, false));
+		}
+	}
+
+	private IEnumerator PlayAnimations(GameObject player, bool startUsingObject)
+	{
+		Animator playerAnimator = player.GetComponent<Animator>();
+		playerAnimator.SetTrigger(startUsingObject ? startPlayerAnimationTriggerName : endPlayerAnimationTriggerName);
+
+		if(!startUsingObject)
+		{
+			while(!playerAnimator.GetCurrentAnimatorStateInfo(0).IsName("Idle0"))
+			{
+				yield return null; // Wait for the Idle animation.
+			}
+		}
+
+		player.GetComponent<NavMeshAgent>().enabled = !startUsingObject;
+		playerAnimator.applyRootMotion = startUsingObject;
+
+		if(startUsingObject)
+		{
+			player.transform.rotation = Quaternion.Euler(0, startPlayerAnimationRotation, 0);
+		}
+
+		player.GetComponent<PlayerInteraction>().IsPlayerUsingObject = startUsingObject;
+	}
+
+	protected virtual void PlayOtherAnimations()
+	{
+
+	}
+}
